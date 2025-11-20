85f059f9144e9b0715853a101844afd3a5c7f9d4
johaeun301 <haeunj125@gmail.com>
2025-11-20 13:48:35 +0900

    도서관 프로그램 v1.0 - Main 추가

--- Java-Library/src/Main.java
+++ Java-Library/src/Main.java
@@ -1,13 +1,40 @@
-//TIP 코드를 <b>실행</b>하려면 <shortcut actionId="Run"/>을(를) 누르거나
-// 에디터 여백에 있는 <icon src="AllIcons.Actions.Execute"/> 아이콘을 클릭하세요.
-void main() {
-    //TIP 캐럿을 강조 표시된 텍스트에 놓고 <shortcut actionId="ShowIntentionActions"/>을(를) 누르면
-    // IntelliJ IDEA이(가) 수정을 제안하는 것을 확인할 수 있습니다.
-    IO.println(String.format("Hello and welcome!"));
+import java.util.*;
 
-    for (int i = 1; i <= 5; i++) {
-        //TIP <shortcut actionId="Debug"/>을(를) 눌러 코드 디버그를 시작하세요. 1개의 <icon src="AllIcons.Debugger.Db_set_breakpoint"/> 중단점을 설정해 드렸습니다
-        // 언제든 <shortcut actionId="ToggleLineBreakpoint"/>을(를) 눌러 중단점을 더 추가할 수 있습니다.
-        IO.println("i = " + i);
+// 메인 클래스
+public class LibraryProgram {
+    private static Scanner sc = new Scanner(System.in);
+
+    private static BookInput input = new BookInput();
+    private static BookSearch search = new BookSearch();
+    private static BookCategoryUpdater updater = new BookCategoryUpdater();
+
+    public static void main(String[] args) {
+        while (true) {
+            System.out.println("===== 도서관 프로그램 =====");
+            System.out.println("1. 도서 정보 입력");
+            System.out.println("2. 도서 검색");
+            System.out.println("3. 도서 카테고리 수정");
+            System.out.println("4. 종료");
+            System.out.print("메뉴 선택 : ");
+
+            int choice = Integer.parseInt(sc.nextLine());
+
+            switch (choice) {
+                case 1:
+                    input.addBook();
+                    break;
+                case 2:
+                    search.searchBook();
+                    break;
+                case 3:
+                    updater.updateCategory();
+                    break;
+                case 4:
+                    System.out.println("프로그램 종료");
+                    return;
+                default:
+                    System.out.println("잘못된 입력입니다.");
+            }
+        }
     }
 }
