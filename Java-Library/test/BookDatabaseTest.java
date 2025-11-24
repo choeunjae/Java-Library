f4a544535bc3a5ac8dde429016b2da89ce6f0be9
cho eun jae <jej059813@naver.com>
2025-11-24 21:46:18 +0900

    통합 단위 테스트 완료

---
+++ Java-Library/test/BookDatabaseTest.java
@@ -0,0 +1,18 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BookDatabaseTest {
+
+    @Test
+    void saveBook() {
+    }
+
+    @Test
+    void getBooks() {
+    }
+
+    @Test
+    void updateCategoryByTitle() {
+    }
+}
